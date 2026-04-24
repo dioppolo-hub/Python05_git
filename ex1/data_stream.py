d8382d0a609fc0e13d9727fac51dea1de078fd83
dioppolo <diego.ioppolo2006@gmail.com>
2026-04-24 12:17:49 +0200

    ex1 temp

--- ex1/data_stream.py
+++ ex1/data_stream.py
@@ -127,16 +127,90 @@ class LogProcessor(DataProcessor):
 
 
 class DataStream():
-    def __init__(self)
+    def __init__(self):
+        self.ProcMem = []
+        self.textStream = 0
+        self.numStream = 0
+        self.logStream = 0
+        self.sizeStream = 0
 
 
     def register_processor(self, proc: DataProcessor) -> None:
+        if isinstance(proc, DataProcessor):
+            self.ProcMem.append(proc)
+            if isinstance(proc, NumericProcessor):
+                print("Registering NumericProcessor\n")
+            elif isinstance(proc, TextProcessor):
+                print("Registering TextProcessor\n")
+            elif isinstance(proc, LogProcessor):
+                print("Registering LogProcessor\n")
+            else:
+                print("No Processor Found, no data\n")
 
 
-    def process_stream(self, stream: list[typing.Any]) -> None:
+    def process_stream(self, stream: list[Any]) -> None:
+        for item in stream:
+            handled = False
+            self.sizeStream += 1
+            for proc in self.ProcMem:
+                if proc.validate(item):
+                    proc.ingest(item)
+                    if isinstance(proc, NumericProcessor):
+                        self.numStream += 1
+                    elif isinstance(proc, TextProcessor):
+                        self.textStream += 1
+                    elif isinstance(proc, LogProcessor):
+                        self.logStream += 1
+                    handled = True
+            if not handled:
+                print(
+                    "DataStream error "
+                    f"- Can't process element in stream: {item}"
+                )
 
 
     def print_processors_stats(self) -> None:
+        Tstream = self.textStream
+        Nstream = self.numStream
+        Lstream = self.logStream
+        Sstream = self.sizeStream
+        for proc in self.ProcMem:
+            if isinstance(proc, NumericProcessor):
+                NotPstream = Sstream - Nstream
+                print(
+                    f"Numeric Processor: total {Nstream} "
+                    f"item processed, remaining {NotPstream} on processor"
+                )
+            elif isinstance(proc, TextProcessor):
+                NotPstream = Sstream - Tstream
+                print(
+                    f"Text Processor: total {Tstream} "
+                    f"item processed, remaining {NotPstream} on processor"
+                )
+            elif isinstance(proc, LogProcessor):
+                NotPstream = Sstream - Lstream
+                print(
+                    f"Log Processor: total {Lstream} "
+                    f"item processed, remaining {NotPstream} on processor"
+                )
+        print(f"Resume: Numeric {Nstream}, Text: {Tstream}, Log {Lstream}")
 
 
 if __name__ == "__main__":
+    print("=== Code Nexus - Data Strem ===\n")
+    ds = DataStream()
+    ds.register_processor(NumericProcessor())
+    stream = [
+        "Hello Word",
+        42,
+        [34, 56, 78],
+        {"5": "sword"},
+        ["x", "y"],
+        3.14
+    ]
+    ds.register_processor(LogProcessor())
+    ds.register_processor(TextProcessor())
+    ds.process_stream(stream)
+    print(f"Send first batch of data: {stream}")
+    print("=== DataStress statistic ===\n")
+    ds.print_processors_stats()
